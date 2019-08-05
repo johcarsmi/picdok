edcb609f6c5cffc4c65c2725371dd25a034ec4be
John Carrick Smith <jcarricksmith@gmail.com>
2019-08-05 18:04:06 +0100

    Removed commented out code

--- src/picdok.cpp
+++ src/picdok.cpp
@@ -471,30 +471,7 @@ void Picdok::doResize()     // Resize QPixMap to match the size of the label so 
     fWdth = ui->lblPic->width();
     ui->lblPic->setPixmap(pixmDisp->scaled(fWdth,fHt,Qt::KeepAspectRatio,Qt::SmoothTransformation));
 }
-/*
-bool Picdok::getExifData(const QString & inFile, QString &retComm, QString &retOrtn, QString &retDate)
-// Get the UserComment, Orientation and DateTimeOriginal from the EXIF data.
-{
-    Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(inFile.toUtf8().toStdString()); // ImageFactory::open() expects a std::string() value.
-    image.get();
-    image->readMetadata();
-    Exiv2::ExifData &exifData = image->exifData();
-    if (exifData.empty()) return false;
-    Exiv2::Exifdatum &datum = exifData["Exif.Photo.UserComment"];
-    retComm = datum.toString().c_str();
-    if (retComm.startsWith("charset="))     // This test is probably never true, but left in just in case.
-    {
-        int spos = retComm.indexOf(' ');
-        retComm = retComm.remove(0,spos+1);
-    }
-    retComm = retComm.trimmed();        // Some cameras populate the UserComment with spaces.
-    datum = exifData["Exif.Photo.DateTimeOriginal"];
-    retDate = datum.toString().c_str();
-    datum = exifData["Exif.Image.Orientation"];
-    retOrtn = datum.toString().c_str();
-    return true;
-}
-*/
+
 void Picdok::doSave()     // Save the revised UserComment data back to the source file.
 {
     WaitPtr(true);
